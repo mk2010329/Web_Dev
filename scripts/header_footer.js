63d56a27a899dbdfc655d42dc48763752595bf21
mohammad-mk2010329 <hassamtk@gmail.com>
2024-03-21 02:19:48 +0300

    almost done with the home page. Also added header_footer script

---
+++ scripts/header_footer.js
@@ -0,0 +1,14 @@
+
+fetch('header.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('header').innerHTML = html;
+    });
+
+
+
+fetch('footer.html')
+.then(response => response.text())
+.then(html => {
+    document.getElementById('footer').innerHTML = html;
+});
