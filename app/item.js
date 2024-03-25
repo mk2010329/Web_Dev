6f5045650761054d63bfd7928c5f7e99d40037fd
mohammad-mk2010329 <hassamtk@gmail.com>
2024-03-25 14:21:49 +0300

    made items.js, seller.js, itemsRepo.js

---
+++ app/item.js
@@ -0,0 +1,12 @@
+export class Item{
+    constructor(name , price ,boughtByUser, quantity, picture,category){
+        this.name=name;
+        this.pice=price;
+        this.boughtByUser=boughtByUser;
+        this.quantity=quantity
+        this.picture=picture;
+        this.category=category;
+
+    }
+    
+}
---
+++ app/repo/ItemsRepo.js
@@ -0,0 +1,22 @@
+const items = []
+
+function uploadItem(item){
+
+}
+
+function searchItem(item){
+
+}
+
+function getItems(){
+    return items;
+}
+
+
+function storeItem(){
+
+}
+
+function purchaseItem(item){
+    
+}
