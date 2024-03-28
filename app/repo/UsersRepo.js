23ac7cbc024006023d1295a29c587fb53d53f123
Real-Subzi <ss2105006@qu.edu.qa>
2024-03-28 21:50:47 +0300

    Completed login.js

--- app/repo/UsersRepo.js
+++ app/repo/UsersRepo.js
@@ -1,5 +1,24 @@
-const users = []
+export let users = []
+let loggedInUser = {}
 
-function findUser(username, password) {
-    
+document.addEventListener('DOMContentLoaded', domHandler);
+async function domHandler(){
+    const userDataResponse = await fetch('app/data/users.json');
+    const userDataArray = await userDataResponse.json();
+    users = userDataArray;
+    console.log(users);
+}
+
+export function findUser(username, password) {
+    const foundUser = users.find((user)=>user.username==username && user.password==password);
+    if(foundUser === undefined){
+        return undefined;
+    }else{
+        loggedInUser = foundUser
+        return foundUser;
+    }
+}
+
+function getLoggedInUser() {
+    return loggedInUser;
 }
