d729733e0e075bca88e192601c3a59b2c9a1782f
mohammad-mk2010329 <hassamtk@gmail.com>
2024-03-29 13:12:10 +0300

    done with "add items to cart"

--- scripts/home_page.js
+++ scripts/home_page.js
@@ -1,5 +1,5 @@
 
-
+let user = []
 const slider = document.querySelector('#main')
 const computer = document.querySelector("#comp-pic")
 const mobile = document.querySelector("#mob-pic")
@@ -19,79 +19,77 @@ async function showComp(){
     slider.style.display='none'
     categories.style.display='none'
     hr.style.display='none'
- //   itemList = localStorage.itemList
-//     if (itemList){
-//         itemList = JSON.parse(localStorage.itemList)
-//         items.innerHTML = itemList.map(item => getItems(item)).join('')
-//     }
-//   else{
+    let computer=""
+   itemList = localStorage.itemList
+    if (itemList){
+        itemList = JSON.parse(localStorage.itemList)
+    }
+  else{
     const data = await fetch('app/data/items.json')
     itemList= await data.json()
-    const computer = itemList.filter(p => p.category=="laptop" )
-    items.innerHTML= computer.map(item => getItems(item)).join("");
-//     localStorage.itemList = JSON.stringify(itemList)
-//   } 
+    
+  } 
+  computer = itemList.filter(p => p.category=="laptop" )
+  localStorage.itemList = JSON.stringify(itemList)
+  items.innerHTML= computer.map(item => getItems(item)).join("");
 }
 
 async function showMobile(){
     slider.style.display='none'
     categories.style.display='none'
     hr.style.display='none'
-//     itemList = localStorage.itemList
-//     if (itemList){
-//         itemList = JSON.parse(localStorage.itemList)
-//         items.innerHTML = itemList.map(item => getItems(item)).join('')
-//     }
-//   else{
+    let mobile =""
+    itemList = localStorage.itemList
+    if (itemList){
+        itemList = JSON.parse(localStorage.itemList)
+    }
+  else{
     const data = await fetch('app/data/items.json')
     itemList= await data.json()
-    const mobile = itemList.filter(p => p.category=="mobile" )
-    items.innerHTML= mobile.map(item => getItems(item)).join("");
-//     localStorage.itemList = JSON.stringify(itemList)
-//   } 
-
+  } 
+  mobile = itemList.filter(p => p.category=="mobile" )
+    localStorage.itemList = JSON.stringify(itemList)
+  items.innerHTML= mobile.map(item => getItems(item)).join("");
 }
 
 async function showAcc(){
     slider.style.display='none'
     categories.style.display='none'
     hr.style.display='none'
-//     itemList = localStorage.itemList
-//     if (itemList){
-//         itemList = JSON.parse(localStorage.itemList)
-//         items.innerHTML = itemList.map(item => getItems(item)).join('')
-//     }
-//   else{
+    let acc =""
+    itemList = localStorage.itemList
+    if (itemList){
+        itemList = JSON.parse(localStorage.itemList)
+    }
+  else{
     const data = await fetch('app/data/items.json')
     itemList= await data.json()
-    const acc = itemList.filter(p => p.category=="accessories" )
+  } 
+   acc = itemList.filter(p => p.category=="accessories" )
     items.innerHTML= acc.map(item => getItems(item)).join("");
-//     localStorage.itemList = JSON.stringify(itemList)
-//   } 
+    localStorage.itemList = JSON.stringify(itemList)
 
 }
 
-async function addToCart(item){
-    console.log(item);
-    const data = await fetch('app/data/users.json')
-    const users =  await data.json()
-    const loggedInUser = users.find(user => user.username==2)
-    loggedInUser.cart.itemsInCart.unshift(item)
-    localStorage.userList = JSON.stringify(loggedInUser)
-//     cart.innerHTML += 
-//      `
-//     <div class ="item-card">
-//     <img src="${item.picture}">
-//     <div>
-//     <p><b>Title:</b> ${item.name} </p>
-//     <p><b>Price:</b> ${item.price} QAR </p>
-//     <p><b>Quantity:</b> ${item.quantity} </p>
-//     </div>
-//     <div>
-//     <button  class="add-btn" onclick="addToCart(${item})">Remove From Cart</button>
-//     </div>
-//     </div>
-// `
+async function addToCart(id){
+    
+    let loggedInUser = localStorage.userList
+    let users=""
+    if(loggedInUser){
+        loggedInUser = JSON.parse(localStorage.userList)
+      
+    }else{
+        const data = await fetch('app/data/users.json')
+        users =  await data.json()
+        loggedInUser = users.find(user => user.username==2)
+         
+    }
+    itemList = JSON.parse(localStorage.itemList)
+    const item = itemList.find(i => i.id == id)
+    loggedInUser.cart.push(item)
+    localStorage.userList = JSON.stringify(loggedInUser)   
+    window.alert(item.name + " is add to your cart!");
+
 }
 
 function getItems(item){
@@ -104,7 +102,7 @@ function getItems(item){
     <p><b>Quantity:</b> ${item.quantity} </p>
     </div>
     <div>
-    <button  class="add-btn" onclick="addToCart('${item}')">Add to Cart</button>
+    <button  class="add-btn" onclick="addToCart('${item.id}')">Add to Cart</button>
     </div>
     </div>
 `
--- scripts/purchase.js
+++ scripts/purchase.js
@@ -1,30 +1,49 @@
 
 let cartItems = []
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
-    const cart = document.querySelector("#cart-id")
+    let cart = document.querySelector("#cart-id")
+    let summary = document.querySelector(".summary")
     cartItems = localStorage.userList
-    if(cartItems){
-        console.log("yes data is here");
-    }
     cartItems = JSON.parse(localStorage.userList)
-    console.log(cartItems.cart);
+    let item = cartItems.cart;
+   cart.innerHTML =  item.map(i => addToCart(i)).join("");
+   let cost = []
+   let price = item.map(i =>cost.push(i.price) )
+   let totalCost = cost.reduce((acc,i) => acc+i)
+   let shipping = 20;
+   let Payment = price+ shipping
+    summary.innerHTML = "<br>"+"Total: " + totalCost +" QAR" + "<br>"+"Shipping Charges: " + shipping +" QAR" + "<br>" //+ "Payment: "+ Payment
+    + "<br>"+ payBtn()
+})
 
-
-        cart.innerHTML += 
-     `
+function addToCart(item){
+    return `
     <div class ="item-card">
-    <img src="${cartItems.picture}">
+    <img class="item_img" src="${item.picture}">
     <div>
-    <p><b>Title:</b> ${cartItems.name} </p>
-    <p><b>Price:</b> ${cartItems.price} QAR </p>
+    <p><b>Title:</b> ${item.name} </p>
+    <p><b>Price:</b> ${item.price} QAR </p>
     </div>
     <div>
-    <button  class="add-btn" >Remove From Cart</button>
+    <button  class="add-btn" ><span>&minus;</span></button>
     </div>
     </div>
 ` 
-})
+}
 
+function payBtn(){
+    return `
+    <button  class="pay-btn" >Pay Now</span></button>
+    `
+}
+
+function removeItem(id) {
+    const book = document.getElementById(`id-${id}`)
+    book.remove()
+    bookList = localStorage.bookList
+    bookList = JSON.parse(localStorage.bookList)
+    const index = bookList.findIndex(book=> book._id==id)
+    bookList.splice(index,1);
+    localStorage.bookList = JSON.stringify(bookList)
+}
